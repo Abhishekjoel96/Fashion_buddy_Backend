2ec0651f5f413c3a2176c868622899eddd319bf5
Abhishek Tadepalli <Abhishekjoel58@gmail.com>
2025-03-08 16:34:09 +0530

    Update clients.js

--- backend/api/clients.js
+++ backend/api/clients.js
@@ -1,13 +1,23 @@
 // backend/api/clients.js
 require('dotenv').config();
 const express = require('express');
-const app = express();
-const clientController = require('../src/controllers/clientController');
 const { authenticateAdmin, validateFields } = require('../src/middleware/auth');
+const clientController = require('../src/controllers/clientController');
 const { corsHeaders } = require('./_shared/cors');
+
+const app = express();
 
 // Middleware
 app.use(express.json());
+
+// Enable CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', corsHeaders['Access-Control-Allow-Origin']);
+  res.setHeader('Access-Control-Allow-Methods', corsHeaders['Access-Control-Allow-Methods']);
+  res.setHeader('Access-Control-Allow-Headers', corsHeaders['Access-Control-Allow-Headers']);
+  res.setHeader('Access-Control-Max-Age', corsHeaders['Access-Control-Max-Age']);
+  next();
+});
 
 // Routes
 app.get('/', authenticateAdmin, clientController.getAllClients);
@@ -17,11 +27,12 @@ app.post('/', authenticateAdmin, validateFields(['phone_number']), clientControl
 app.put('/:id', authenticateAdmin, clientController.updateClient);
 app.delete('/:id', authenticateAdmin, clientController.deleteClient);
 
+// OPTIONS Handling in the middleware.
+app.options('*', (req, res) => {
+  res.status(204).end();
+});
+
+// Vercel handler
 module.exports = (req, res) => {
-  if (req.method === 'OPTIONS') {
-    res.writeHead(204, corsHeaders);
-    res.end();
-    return;
-  }
   app(req, res);
 };
