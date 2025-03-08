d672d31fff4fc4fda93cadcb6a323ff3b3c08881
Abhishek Tadepalli <Abhishekjoel58@gmail.com>
2025-03-08 14:12:23 +0530

    Create cors.js

---
+++ backend/api/_shared/cors.js
@@ -0,0 +1,9 @@
+// backend/api/_shared/cors.js
+module.exports = {
+  corsHeaders: {
+    'Access-Control-Allow-Origin': 'https://fashion-buddy-chat-jjalx6jip-abhisheks-projects-76c99680.vercel.app',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization, x-api-key',
+    'Access-Control-Max-Age': '86400'
+  }
+};
