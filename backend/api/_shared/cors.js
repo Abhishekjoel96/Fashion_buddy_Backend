a955106dfc8989e2dade8538375867ab2a737a35
Abhishek Tadepalli <Abhishekjoel58@gmail.com>
2025-03-10 11:54:04 +0530

    Delete backend/api/_shared directory

--- backend/api/_shared/cors.js
+++
@@ -1,9 +0,0 @@
-// backend/api/_shared/cors.js
-module.exports = {
-  corsHeaders: {
-    'Access-Control-Allow-Origin': 'https://fashion-buddy-chat.vercel.app',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization, x-api-key',
-    'Access-Control-Max-Age': '86400'
-  }
-};
